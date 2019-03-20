e775491cfd4412b78ab5eb78ef9b6e2dd6e16991
m-j-hacker <mjhacker@gmail.com>
2019-03-20 17:28:30 -0500

    Create server.js with basic GET endpoint

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// sanity check endpoint
+server.get('/', (req, res) => {
+    res.send("It's allliiiiiive!!");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3333;
+}
+
+server.listen(port, () => console.log(`***API running on ${port}***`));
