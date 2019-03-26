ff81e60bc39610cece12d8aafeba361ec7f680bf
sean-one <basement365@gmail.com>
2019-03-26 10:36:20 -0700

    added find user by id end point

--- api/users/index.js
+++ api/users/index.js
@@ -13,4 +13,25 @@ router.get('/', (req, res) => {
   .catch(err => res.status(500).json(err));
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db
+  .findById(id)
+  .then(user => {
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'user not found'})
+    }
+  })
+  .catch(err => {
+    res
+    .status(500)
+    .json({ message: "we failed you", error: err })
+  })
+});
+
+
+
 module.exports = router;
