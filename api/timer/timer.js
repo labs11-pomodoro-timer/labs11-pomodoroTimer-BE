2304f9634ebdb10032d7b846615d1f4588c16d14
m-j-hacker <mjhacker@gmail.com>
2019-03-29 15:35:16 -0500

    Create timer component, update server.js

--- api/timer/timer.js
+++ api/timer/timer.js
@@ -1,28 +1,52 @@
-const focusTime = 25 * 60;
-
-let time = focusTime;
-let timerRunning = false;
-let interval;
-let timeLeft;
+// Countdown timer function that takes in a time argument and
+// sets the timer based on the input of the API endpoint
 
 
+function countDown(time) {
+    let initialTime;
+    let timerRunning = true;
 
-function countDown() {
-    interval = setInterval(function(){
+    if(time === "short"){
+        initialTime = 5*60;
+    }else if(time === "long"){
+        initialTime = 15*60;
+    }else if(time === "focus"){
+        initialTime = 25*60;
+    }else if(isNaN(time) === false){
+        initialTime = parseInt(time);
+    }
+    
+    setInterval(function(){
+      if (time === 0) {
+        console.log("The timer has finished") 
+      }
+      if (timerRunning) {
         time--;
-        if (time < 0) {
-            setStartTime();
-        }
-        timeLeft = focusTime - time;
-    }, 1000);
-}
+        
+            if (time > 0) {
+                console.log(time)
+            } else {
+              timerFinished()
+               
+            }
+        } 
+        }, 1000);
 
-function timerReport() {
+}
+      
+
+
+function timerReport(time) {
     let seconds = time % 60;
     let minutes = ((time / 60) | 0);
     return "The user is in Focus mode for " + minutes + ":" + seconds;
 }
 
-function setStartTime() {
-    time = focusTime;
+function timerFinished() {
+    // Execute once timer reaches 0. User should be taken out of Focus
+    // mode on Slack
+    
+    timerRunning = false;
+    
+    
 }
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const app = express();
 const usersRouter = require('./api/users/index.js');
 const slackUsersRouter = require('./api/slackUsers/index.js');
+const pomodoroTimerRouter = require('./api/timer/index.js');
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/users', usersRouter);
 app.use('/api/slackUsers', slackUsersRouter);
+app.use('/api/timer', pomodoroTimerRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({ api: 'alive' });
