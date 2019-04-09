f69ec1c7b4be36a8667e8c2540ad08d0cfeeda44
m-j-hacker <mjhacker@gmail.com>
2019-04-09 11:18:30 -0500

    Build basic functions needed for slash commands

--- api/routes/slashRoutes.js
+++ api/routes/slashRoutes.js
@@ -2,3 +2,62 @@ require("dotenv").config();
 const express = require("express");
 const db = require("../../data/dbslackUserModel");
 const request = require("request");
+
+// We will need bodyParser to communicate well with Slack
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// HOF that takes in the uri and json message for a slack message
+function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
+    let postOptions = {
+      uri: responseURL,
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      json: JSONmessage
+    };
+    request(postOptions, (error, response, body) => {
+      if (error) {
+        // handle errors as you see fit
+        res.json({ error: "Error." });
+      }
+    });
+  };
+
+  // This function is for posting normal messages
+  function postMessage(JSONmessage, token) {
+    let postOptions = {
+      uri: `https://slack.com/api/chat.postMessage`,
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      json: JSONmessage
+    };
+    request(postOptions, (error, response, body) => {
+      if (error) {
+        // Error handling
+        res.json({ error: "Error." });
+      }
+    });
+  };
+
+  // This function is for posting ephemeral messages
+  function postEphMessage(JSONmessage, token) {
+    let postOptions = {
+      uri: `https://slack.com/api/chat.postEphemeral`,
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      json: JSONmessage
+    };
+    request(postOptions, (error, response, body) => {
+      if (error) {
+        // Error handling
+        res.json({ error: "Error." });
+      }
+    });
+  };
