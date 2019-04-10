2a37a769a38735d8f89e7751c897a672d9f53644
sean-one <basement365@gmail.com>
2019-04-09 17:56:09 -0700

    added users table

---
+++ data/migrations/20190409175417_users.js
@@ -0,0 +1,36 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+
+        users
+            .string('firstname', 20)
+            .notNullable();
+
+        users
+            .string('lastname', 20)
+            .notNullable();
+
+        users
+            .string('email', 128)
+            .notNullable()
+            .unique();
+
+        users
+            .integer('phone', 10);
+
+        users
+            .string('timerName', 30)
+
+        users
+            .integer('timerStart', 20);
+
+        users
+            .integer('timerEnd', 20);
+
+    })
+
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+};
