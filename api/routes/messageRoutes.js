40e46dec7627e5f48b722c0c3af0265d4f1aab3a
sean-one <basement365@gmail.com>
2019-04-09 17:52:54 -0700

    updated a require

--- api/routes/messageRoutes.js
+++ api/routes/messageRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../../data/timerModel");
+const db = require("../../data/dbModel");
 
 // message-based endpoints here6485
 
