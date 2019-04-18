13a267be2b11e6d901a6bfc931bc308054df2e38
Steven O'Neill <oneillsteven957@gmail.com>
2019-04-18 11:22:27 -0500

    testing for database

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
-const dbEngine = process.env.DB_ENGINE || "production";
+const dbEngine = process.env.DB_ENGINE || "development";
 const config = require("../knexfile.js")[dbEngine];
 
 module.exports = require("knex")(config);
